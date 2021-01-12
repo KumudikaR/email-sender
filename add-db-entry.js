4b1dd2c749563a13ef502c3ae44a1bbb1a7d933e
KumudikaR <kumudikarupasinghe@gmail.com>
2021-01-12 18:58:12 +0530

    email sender

---
+++ add-db-entry.js
@@ -0,0 +1,26 @@
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+
+    let name = event['Name']
+    let email = event['Email']
+    let feedback = event['Feedback']
+
+    try {
+        let data = await ddb.put({
+            TableName: "feedback",
+            Item: {
+                Name: name,
+                Email: email,
+                Feedback: feedback
+            }
+        }).promise();
+        console.log("Successfully Saved entry from email:", email);
+
+    } catch (err) {
+       console.log("Failed to save entry from email:", email, err);
+    };
+
+    return { "message": "Successfully executed" };
+};
---
+++ send-email.js
@@ -0,0 +1,45 @@
+/* 
+1) Replace 'Source' email address with your Amazon SES-verified sender email address in the same AWS account and Region.
+2) Click Deploy button on the Toolbar.
+3) Once the deployment is successful, copy the API endpoint URL from the output parameter section of Deployment Summary 
+view or via the Deployment tab of Project → Show Info.
+4) Using an HTTP client, make a POST request to the above API endpoint URL.
+(Refer https://www.slappforge.com/blog/email-triggers-when-a-new-record-is-created-in-dynamodb for more details)
+*/
+const AWS = require('aws-sdk');
+const ses = new AWS.SES();
+
+exports.handler = async (event) => {
+
+    let tabledetails = event.Records[0].dynamodb;
+    console.log(tabledetails);
+
+    let name = tabledetails.NewImage.Name.S;
+    let email = tabledetails.NewImage.Email.S;
+    let messagebody = 'Hi' + ' ' + name + '! Thank you for your feedback'
+
+    try {
+        let data = await ses.sendEmail({
+            Source: "sender@example.com",
+            Destination: {
+                ToAddresses: [email]
+            },
+            Message: {
+                Subject: {
+                    Data: "Thank you for your feedback!"
+                },
+                Body: {
+                    Text: {
+                        Data: messagebody
+                    }
+                }
+            }
+        }).promise();
+        console.log("Sent email to", email)
+
+    } catch (err) {
+        console.log("Email sending failed", err)
+    };
+
+    return { "message": "Successfully executed" };
+};
